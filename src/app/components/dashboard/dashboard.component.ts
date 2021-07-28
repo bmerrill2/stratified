1061a1e262241ea9322624fb5d50abf42c667dca
Brad Merrill <bradmerrill@eighty1designs.com>
2021-07-28 03:31:29 -0600

    Dashboard Update

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface ProductTable {
+  product: string;
+  percentage: string;
+}
+
+const PRODUCT_DATA:ProductTable[] = [
+    {product: 'Product 1', percentage: '89%'},
+    {product: 'Product 2', percentage: '89%'},
+    {product: 'Product 3', percentage: '89%'},
+    {product: 'Product 4', percentage: '89%'},
+    {product: 'Product 5', percentage: '89%'},
+    {product: 'Product 6', percentage: '89%'},
+    {product: 'Product 7', percentage: '89%'},
+];
+    
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+    
+displayedColumns: string[] = ['product', 'percentage'];
+dataSource = PRODUCT_DATA;
 
   constructor() { }
 
